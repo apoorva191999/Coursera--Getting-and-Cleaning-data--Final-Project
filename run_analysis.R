ac2c41b23059a4db472c198632cb4a183b60f50d
apoorva191999 <71211031+apoorva191999@users.noreply.github.com>
2020-10-16 21:31:55 +0530

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+#run_analysis
+
+#1)downloading the raw data dile from the link
+
+dataurl<- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url=dataurl, destfile = "c:/users/hp/documents/project1")
+unzip(zipfile = "project1")
+
+#2) Creating dataset containing the train data.
+x_train<-read.table("UCI HAR Dataset/train/X_train.txt")
+y_train<- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train<- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+#3) Creating dataset containing the test data
+x_test<- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test<- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test<- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+#4) Merge the train and test datasets.
+test<- cbind(subject_test, y_test, x_test)
+train<- cbind(subject_train,y_train, x_train)
+subject_data<- rbind(subject_train, subject_test)
+alldata<- rbind(test,train)
+
+
+#5) Loading features data
+feature<- read.table("UCI HAR Dataset/features.txt")
+
+#6) Loading activity data
+activity<- read.table("UCI HAR Dataset/activity_labels.txt")
+colnames(alldata)<- c("Subject", "Activity", feature[,2]) 
+
+#7)Extracting mean and std measurements.
+selected<-grep("(mean|std)", feature$V2, value = TRUE)
+columnstokeep<- grepl("subject|activity|mean|std", colnames(alldata))
+alldata<- alldata[,columnstokeep]
+
+
+#8) Using descriptive names for the actvities.
+alldata$Activity <- factor(alldata$Activity, levels = activity[,1], labels = activity[,2])
+alldata$Subject <- as.factor(alldata$Subject)
+
+#9)Producing the tidy data
+meltdata<- melt(alldata, id=c("Subject", "Activity"))
+finaldata<- dcast(meltdata,  Subject + Subject ~ variable, mean)
+
